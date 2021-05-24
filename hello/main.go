f7660cd1d1d46c332de54902b8322cdf976ee8c5
d-sauer <davor.sauer@gmail.com>
2021-05-24 11:58:35 +0200

    check func variables assignemnt

--- hello/main.go
+++ hello/main.go
@@ -4,4 +4,22 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, world.")
+	variables()
+}
+
+func variables() {
+	x := 10
+	fmt.Printf("x: %d\n", x)
+
+	x = 15
+	fmt.Printf("x: %d\n", x)
+
+	// Not allowed
+	//x := 20
+	//fmt.Printf("x: %d\n", x)
+
+	// allowed `:=`
+	// as long as there is one new variable on the lefthand side of := then any of the other variables can already exist
+	x, y := 20, 25
+	fmt.Printf("x: %d, y: %d\n", x, y)
 }
