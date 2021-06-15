001f980855b2da76f44acc730bd31eee0a57f4cc
d-sauer <davor.sauer@gmail.com>
2021-06-15 23:42:35 +0200

    Explore Go interfaces and mock for testing.

--- hello-interface/main.go
+++ hello-interface/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	user := u.User{"Neil", "Young"}
+	user := &u.User{"Neil", "Young"}
 	fmt.Printf("First Name: %s, Last Name: %s \n", user.FirstName, user.LastName)
 	fmt.Printf("First & Lase Name len: %d \n", u.GetFirstLastNameLength(user))
 }
