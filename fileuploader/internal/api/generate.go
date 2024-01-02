f823107ebaddcada0d2f33aed829a184f7612a77
d-sauer <davor.sauer@gmail.com>
2024-01-02 20:57:47 +0100

    FileUploader init

---
+++ fileuploader/internal/api/generate.go
@@ -0,0 +1,4 @@
+package api
+
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen --config server.cfg.yaml ../../api/openapi3.yaml
+////go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config server.cfg.yaml ../../api/openapi3.yaml
